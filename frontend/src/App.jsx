f636fcc49cf66dd7932e08f44c4b62ffccee8bec
aemmy5409 <aemmy360@gmail.com>
2025-05-26 20:31:26 +0100

    worked on  the order history

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import {
 	Route,
 	Navigate,
 } from "react-router-dom";
+import OrderHistory from "./pages/OrderHistory";
 
 function App() {
 	return (
@@ -27,6 +28,7 @@ function App() {
 					<Route exact path="/register" element={<Register />} />
 					<Route exact path="/checkout" element={<Checkout />} />
 					<Route exact path="/placeorder" element={<PlaceOrder />} />
+					<Route exact path="/order-history" element={<OrderHistory />} />
 				</Routes>
 			</Router>
 		</>
